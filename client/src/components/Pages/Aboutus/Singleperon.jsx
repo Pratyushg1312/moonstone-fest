cac2eeecca3d398d4910a4deaacca5695e90a9e1
sharma39vishal <sharma39vishal@gmail.com>
2023-02-13 23:53:56 +0530

    about us page

---
+++ client/src/components/Pages/Aboutus/Singleperon.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Singleperon() {
+  return (
+    <figure class="snip1527">
+    <div class="image"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/pr-sample24.jpg" alt="pr-sample24" /></div>
+    <figcaption>
+      <div class="date"><span class="day">17</span><span class="month">Nov</span></div>
+      <h3>An Abstract Post Heading</h3>
+      <p>
+  
+        Sometimes the surest sign that intelligent life exists elsewhere in the universe is that none of it has tried to contact us.
+      </p>
+    </figcaption>
+    <a href="#"></a>
+  </figure>
+  )
+}
