db46d9ed8cc554067c4173780eaf2c07865c28f1
sharma39vishal <sharma39vishal@gmail.com>
2023-02-13 22:24:42 +0530

    routers and pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import { Route, Routes } from "react-router-dom";
 import "./App.css"
+import Home from "./components/Pages/Home/Home.jsx"
+import Aboutus from "./components/Pages/Aboutus/Aboutus.jsx"
+import Header from "./components/Header/Header.jsx"
+import Footer from "./components/Footer/Footer.jsx"
 function App() {
   return (
     <div >
-      App js
+      <Header/>
+      <Routes>
+        <Route exact path='/' element={<Home/> }/>
+        <Route  path='/aboutus' element={<Aboutus/> }/>
+      </Routes>
+      <Footer/>
     </div>
   );
 }
