c24f2e75a4dcd2767343e94deb07afeddf90d5fd
sharma39vishal <sharma39vishal@gmail.com>
2023-02-14 19:19:40 +0530

    about us photos

--- client/src/components/Pages/Aboutus/Aboutus.jsx
+++ client/src/components/Pages/Aboutus/Aboutus.jsx
@@ -10,13 +10,16 @@ export default function Aboutus() {
       </div>
 
       <div className="aboutus-container">
-        <h1>About Us</h1>
+        <h1>About MediCaps</h1>
       </div>
       <div className="aboutus-content">
           <img className='aboutus-main-img' src="https://www.dialeducation.com/assets/images/gallery/gallery_1625127758.jpg" alt="" />
         <div className="aboutus-side-description">
           <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis velit labore placeat reprehenderit quidem omnis aliquam porro praesentium earum enim. Sapiente, quam. Doloribus sit est cupiditate, doloremque ut harum quisquam perferendis. Repellat, eum vero. Omnis accusamus sed odio, sequi facilis minus sint reiciendis nihil deserunt dolor exercitationem, quasi numquam et soluta modi vel impedit consectetur. Vitae at illo eveniet nihil modi consequatur laudantium? At perspiciatis unde nulla voluptate placeat corrupti non, asperiores facere ipsam distinctio error obcaecati nemo voluptates ipsum fugit omnis. Tempora recusandae repellendus veritatis corrupti iusto nihil minus, inventore minima cum necessitatibus quae enim iste consectetur nesciunt ab.</p>
         </div>
+      </div>
+      <div className="aboutus-container">
+      <h1>Offical Persons</h1>
       </div>
       <div className="person">
       <Persons/>
--- client/src/components/Pages/Aboutus/Singleperon.jsx
+++ client/src/components/Pages/Aboutus/Singleperon.jsx
@@ -1,18 +1,11 @@
 import React from 'react'
 
-export default function Singleperon() {
+export default function Singleperon(props) {
   return (
-    <figure class="snip1527">
-    <div class="image"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/pr-sample24.jpg" alt="pr-sample24" /></div>
-    <figcaption>
-      <div class="date"><span class="day">17</span><span class="month">Nov</span></div>
-      <h3>An Abstract Post Heading</h3>
-      <p>
-  
-        Sometimes the surest sign that intelligent life exists elsewhere in the universe is that none of it has tried to contact us.
-      </p>
-    </figcaption>
-    <a href="#"></a>
-  </figure>
+  <div className='box'>
+    <div class="box-image"><img width={"450px"} src={props.img} alt="pr-sample24" /></div>
+      <h3>{props.name}</h3>
+      <p>{props.designation}</p>
+  </div>
   )
 }
