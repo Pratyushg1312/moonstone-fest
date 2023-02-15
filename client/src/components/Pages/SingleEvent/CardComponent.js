fabe764a5d18dde5bece8406905be87a4679af05
Kalash Jain <kalashjain446@gmail.com>
2023-02-15 22:57:50 +0530

    modified event page

---
+++ client/src/components/Pages/SingleEvent/CardComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './SingleEvent.css'
+export default function CardComponent() {
+  return (
+    <>
+    <div className='card'>
+      <h1 className='text-center'>Event 1</h1>
+    </div>
+    </>
+  )
+}
