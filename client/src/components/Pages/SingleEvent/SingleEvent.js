d9bb43b55d8c4ce491f7808361f92b16b241dfac
Kalash Jain <kalashjain446@gmail.com>
2023-02-18 23:03:00 +0530

    updated single event

--- client/src/components/Pages/SingleEvent/SingleEvent.js
+++ client/src/components/Pages/SingleEvent/SingleEvent.js
@@ -7,17 +7,26 @@ export default function SingleEvent() {
             <div className=' container-fluid p-0' >
                 <div className='d-flex  my-2 align-items-center justify-content-center row'>
                     <video className='col-12' style={{ zIndex: "-1", opacity: ".7", bottom: 0 }} autoPlay muted loop >
-                    <h1 className='text-center' style={{ color: "white" ,position:"absolute"}} >Event Name</h1>
+                        <h1 className='text-center' style={{ color: "white", position: "absolute" }} >Event Name</h1>
                         <source src="/Videos/temp.mp4" type="video/mp4" />
                     </video></div>
-                <div className='my-2'>
+                <div className='my-4'>
                     <h1 className='text-center'>
-                        EVENTS OF THE DAY
+                        DAY EVENTS
                     </h1>
-
+                    <div className='d-flex row'>
+                        <CardComponent image="./images/musicEvent.jpeg"/>
+                        <CardComponent image="./images/bandEvent.jpg"/>
+                        <CardComponent image="./images/singEvent.jpg"/></div>
                 </div>
-                <div className='d-flex row'>
-                <CardComponent /></div>
+                <div className='my-4'>
+                    <h1 className='text-center'>
+                        NIGHT EVENTS
+                    </h1>
+                    <div className='d-flex row'>
+                        <CardComponent image="./images/danceEvent.jpeg" /></div>
+                </div>
+
                 {/* <CardComponent />
                 <CardComponent /> */}
             </div>
