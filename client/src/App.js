947e938409a3a5e25dc0ee48e4a6a9099d5d99a4
sharma39vishal <sharma39vishal@gmail.com>
2023-02-18 15:53:10 +0530

    contactus page

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Home from "./components/Pages/Home/Home.jsx"
 import Aboutus from "./components/Pages/Aboutus/Aboutus.jsx"
 import Header from "./components/Header/Header.jsx"
 import Footer from "./components/Footer/Footer.jsx"
+import Contactus from './components/Pages/Contactus/Contactus';
 function App() {
   return (
     <div >
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route exact path='/' element={<Home/> }/>
         <Route  path='/aboutus' element={<Aboutus/> }/>
+        <Route  path='/contactus' element={<Contactus/> }/>
       </Routes>
       <Footer/>
     </div>
