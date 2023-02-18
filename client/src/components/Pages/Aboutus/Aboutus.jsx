342a7d6b8f477fa1aba9ed1262367549330cb1f4
sharma39vishal <sharma39vishal@gmail.com>
2023-02-18 16:27:53 +0530

    deploy buid

--- client/src/components/Pages/Aboutus/Aboutus.jsx
+++ client/src/components/Pages/Aboutus/Aboutus.jsx
@@ -13,9 +13,12 @@ export default function Aboutus() {
       <div className="aboutus-container">
         <h1>About MediCaps</h1>
       </div>
+
       <div className="aboutus-content">
+         <div >
           <img className='aboutus-main-img' src="https://www.dialeducation.com/assets/images/gallery/gallery_1625127758.jpg" alt="" />
-        <div className="aboutus-side-description">
+         </div>
+        <div className="aboutus-side-description ">
           <h3>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis velit labore placeat reprehenderit quidem omnis aliquam porro praesentium earum enim. Sapiente, quam. Doloribus sit est cupiditate, doloremque ut harum quisquam perferendis. Repellat, eum vero. Omnis accusamus sed odio, sequi facilis minus sint reiciendis nihil deserunt dolor exercitationem, quasi numquam et soluta modi vel impedit consectetur. Vitae at illo eveniet nihil modi consequatur laudantium? At perspiciatis unde nulla voluptate placeat corrupti non, asperiores facere ipsam distinctio error obcaecati nemo voluptates ipsum fugit omnis. Tempora recusandae repellendus veritatis corrupti iusto nihil minus, inventore minima cum necessitatibus quae enim iste consectetur nesciunt ab.</h3>
         </div>
       </div>
--- client/src/components/Pages/Aboutus/Singleperon.jsx
+++ client/src/components/Pages/Aboutus/Singleperon.jsx
@@ -4,8 +4,8 @@ export default function Singleperon(props) {
   return (
   <div className='box'>
     <div class="box-image"><img width={"100%"} style={{maxWidth:"300px", minWidth:"300px"}} src={props.img} alt="pr-sample24" /></div>
-      <h3>{props.name}</h3>
-      <p>{props.designation}</p>
+      <h3 className='mx-2 my-2'>{props.name}</h3>
+      <p className='mx-2 my-2'>{props.designation}</p>
   </div>
   )
 }
