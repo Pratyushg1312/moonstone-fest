90cf001f533a5d516449147aab5fee5f097f6a02
sharma39vishal <sharma39vishal@gmail.com>
2023-02-19 20:11:39 +0530

    registration form working

--- client/src/components/Pages/Registration/Hello.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Hello() {
-  return (
-    <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae consequuntur dolore deserunt itaque eaque ad non corrupti blanditiis dolorem cum harum accusamus veritatis, eius dicta qui, voluptatum praesentium amet vero.</div>
-  )
-}
