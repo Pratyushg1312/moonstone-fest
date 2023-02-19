432ff3e04e90af8b9868bd25cf71cf40ecec95a0
jain-dishika <87193284+jain-dishika@users.noreply.github.com>
2023-02-19 15:10:56 +0530

    event section

--- client/src/App.js
+++ client/src/App.js
@@ -6,9 +6,10 @@ import Aboutus from "./components/Pages/Aboutus/Aboutus.jsx"
 import Header from "./components/Header/Header.jsx"
 import Footer from "./components/Footer/Footer.jsx"
 import Contactus from './components/Pages/Contactus/Contactus';
-import SingleEvent from './components/Pages/SingleEvent/SingleEvent';
+// import SingleEvent from './components/Pages/SingleEvent/SingleEvent';
 import Registration from './components/Pages/Registration/Registration';
-import SingleDay from './components/Pages/SingleEvent/SingleDay';
+import Events from "./components/Pages/Events/Events";
+// import SingleDay from './components/Pages/SingleEvent/SingleDay';
 function App() {
   return (
     <div >
@@ -17,9 +18,10 @@ function App() {
         <Route exact path='/' element={<Home/> }/>
         <Route  path='/aboutus' element={<Aboutus/> }/>
         <Route  path='/contactus' element={<Contactus/> }/>
-        <Route  path='/SingleEvent' element={<SingleEvent/> }/>
+        <Route  path='/events' element={<Events/> }/>
+        {/* <Route  path='/SingleEvent' element={<SingleEvent/> }/> */}
         <Route  path='/registration' element={<Registration/> }/>
-        <Route  path='/SingleEvent' element={<SingleDay/> }/>
+        {/* <Route  path='/SingleEvent/SingleDay' element={<SingleDay/> }/> */}
       </Routes>
       <Footer/>
     </div>
