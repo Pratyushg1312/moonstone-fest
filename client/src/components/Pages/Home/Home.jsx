4c9166f271236aa8e6766cc32e4fe51c35d6e4d1
Kalash Jain <kalashjain446@gmail.com>
2023-02-23 00:48:31 +0530

    sponser section

--- client/src/components/Pages/Home/Home.jsx
+++ client/src/components/Pages/Home/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Hero } from './Hero'
 import Sponsor from './Sponsor'
-
+import NewSponsor from './NewSponsor'
 export default function Home() {
   return (
     <div>
         <Hero/>
       <Sponsor/>
+      {/* <NewSponsor/> */}
     </div>
   )
 }
