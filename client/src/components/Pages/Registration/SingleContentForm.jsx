0db4c20dec15a6de4298ec9c2a140989ca0cd661
sharma39vishal <sharma39vishal@gmail.com>
2023-02-23 22:06:57 +0530

    event page + registration prev and submit

--- client/src/components/Pages/Registration/SingleContentForm.jsx
+++ client/src/components/Pages/Registration/SingleContentForm.jsx
@@ -12,14 +12,16 @@ export default function SingleContentForm(props) {
   }
 
   return (
-    <div>
+<div>
         <div class="typewriter">
             <h1>What is Your {s} ?</h1>
           </div>
           <input type="text" placeholder={s} name={s} value={props.content[s]} onChange={handleChange} required/>
           <div className='btn-left'>
-          <button className='nxt-btn' onClick={()=>{props.setidx(props.idx+1)}}> Next <i class="fa-solid fa-arrow-right"></i> </button>
+          {props.idx!==0?<button className='nxt-btn' onClick={()=>{props.setidx(props.idx-1)}}><i class="fa-solid fa-arrow-left"></i>  Prev </button>:<></>}
+          {props.idx!==3?<button className='nxt-btn' onClick={()=>{props.setidx(props.idx+1)}}> Next <i class="fa-solid fa-arrow-right"></i> </button>:<></>}
+          {props.idx===3?<button className='nxt-btn' onClick={()=>{alert("Submited")}}> Submit<i class="fa-solid fa-arrow-right"></i> </button>:<></>}
         </div>
     </div>
-  )
+    )
 }
