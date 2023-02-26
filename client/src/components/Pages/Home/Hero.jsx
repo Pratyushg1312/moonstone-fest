aa520a7e367116f31644014cb4fcbbff3bd5a08a
Pratyushg1312 <pratyushg095@gmail.com>
2023-02-26 15:24:39 +0530

    heosection completed

--- client/src/components/Pages/Home/Hero.jsx
+++ client/src/components/Pages/Home/Hero.jsx
@@ -1,31 +1,45 @@
-import React from 'react'
-import {Canvas} from '@react-three/fiber'
-import {OrbitControls} from '@react-three/drei'
+import React, { useRef, useState } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import logoOnDemand from './texture/logoOnDemand.png'
+// import{Spheree} from './Spheree'
 import { Model } from './ModelDraco'
-import  { Suspense} from 'react';
+// import {Model} from './3dmoond'
+// import{Model} from'./Nmdraco';
+import { degToRad } from 'three/src/math/MathUtils';
+import { Suspense } from 'react';
 import "./hero.css"
 import { Aboutmoon } from './Aboutmoon';
 export const Hero = () => {
+  const [colorr,setColorr]= useState('cyan');
+ function changecolor(para){
+      setColorr(para);
+  }
+
   return (
-    <div className="Container">
+    <div className="hero-con">
 
-    <div className='hero'>
-       <Canvas camera={{position:[4,7,0]}} >
-          <OrbitControls enableZoom={false} />
-          <ambientLight intensity={1}  color="green" />
-          <ambientLight intensity={1} />
+      <div className='hero'>
+        <Canvas  >
+
+
+
+          {/* <pointLight position={[0,0,0]} intensity={1} color="cyan" castShadow/> */}
+
+          {/* <ambientLight intensity={1} /> */}
           <Suspense fallback={null}>
-            <Model ref={Model} />
+            <Model ref={Model }  colorr={colorr}/>
+            {/* <Model/> */}
           </Suspense>
-        </Canvas>
-        <div className="logo1">
-            <p>MOONSTONE</p>
-        </div>
-    <div className="typ">
 
-        <Aboutmoon/>
-    </div>
-    </div>
+        </Canvas>
+      </div>
+        <div className="logo1" onMouseOver={()=>{ changecolor('red')}}  onMouseOut={()=>{changecolor('cyan')}}>
+          <img src={logoOnDemand}  class="img-fluid rounded mx-auto d-block p-2"  alt="" />
+        </div>
+        <div className="typ" onMouseOver={()=>{ changecolor('red')}}  onMouseOut={()=>{changecolor('cyan')}}>
+
+          <Aboutmoon/>
+        </div>
     </div>
   )
 }
