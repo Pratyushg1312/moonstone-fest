bf419c79fc65a4987b339d0a81def2b46f270d56
Kalash Jain <kalashjain446@gmail.com>
2023-02-26 15:22:41 +0530

    updationi

--- client/src/components/Pages/Aboutus/Aboutus.jsx
+++ client/src/components/Pages/Aboutus/Aboutus.jsx
@@ -10,15 +10,16 @@ export default function Aboutus() {
       <div className='aboutherosection'>
         <Abouthero />
       </div>
-      <div className="text-center my-4">
-        <h1> Medi-Caps University</h1>
-      </div>
-      <div className=" row px-4 mx-4" >
+     
+      <div className=" row " style={{margin: "0px",justifyContent: "center"}} >
         <div className='col-10 col-md-6' >
-          <img  src="https://www.dialeducation.com/assets/images/gallery/gallery_1625127758.jpg" alt="" style={{height:"100%",width:"100%"}}  />
+          <img  src="https://www.dialeducation.com/assets/images/gallery/gallery_1625127758.jpg" alt="university's image" style={{height:"100%",width:"100%"}}  />
         </div>
         <div className="col-10 col-md-6 ">
-          <h5 className="text-justify" style={{color:"#f2d2a8"}}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis velit labore placeat reprehenderit quidem omnis aliquam porro praesentium earum enim. Sapiente, quam. Doloribus sit est cupiditate, doloremque ut harum quisquam perferendis. Repellat, eum vero. Omnis accusamus sed odio, sequi facilis minus sint reiciendis nihil deserunt dolor exercitationem, quasi numquam et soluta modi vel impedit consectetur. Vitae at illo eveniet nihil modi consequatur laudantium? At perspiciatis unde nulla voluptate placeat corrupti non, asperiores facere ipsam distinctio error obcaecati nemo voluptates ipsum fugit omnis. Tempora recusandae .</h5>
+        <div className="text-center my-4">
+        <h1> Medi-Caps University</h1>
+      </div>
+          <h5 className="text-center" style={{color:"#f2d2a8"}}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis velit labore placeat reprehenderit quidem omnis aliquam porro praesentium earum enim. Sapiente, quam. Doloribus sit est cupiditate, doloremque ut harum quisquam perferendis. Repellat, eum vero. Omnis accusamus sed odio, sequi facilis minus sint reiciendis nihil deserunt dolor exercitationem, quasi numquam et soluta modi vel impedit consectetur. Vitae at illo eveniet nihil modi consequatur laudantium? At perspiciatis unde nulla voluptate placeat corrupti non, asperiores facere ipsam distinctio error obcaecati nemo voluptates ipsum fugit omnis. Tempora recusandae .</h5>
         </div>
       </div>
       <div className="text-center my-4">
