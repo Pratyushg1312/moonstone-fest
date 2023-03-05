16b918c88840dfbed598e5e1ea42b875d4bcd86d
sharma39vishal <sharma39vishal@gmail.com>
2023-03-05 20:36:19 +0530

    reg form deploy

--- client/src/components/Pages/Registration/Loginbtn.jsx
+++ client/src/components/Pages/Registration/Loginbtn.jsx
@@ -3,7 +3,7 @@ import './loginbtn.css'
 export default function Loginbtn() {
   return (
 <div class="center">
-      <a href="http://localhost:5000/auth/google" class="google-btn">
+      <a href="/auth/google" class="google-btn">
         <img
           src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
           height={"50px"}
