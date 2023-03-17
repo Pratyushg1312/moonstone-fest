1843b7814053679b0ba0a4c2be9ebe1c39ea7dc1
Pratyushg1312 <pratyushg095@gmail.com>
2023-03-17 23:01:11 +0530

    removed dependencies

--- client/src/components/Pages/Home/Hero.jsx
+++ client/src/components/Pages/Home/Hero.jsx
@@ -1,38 +1,21 @@
 import React, { useRef, useState } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
-import logoOnDemand from './texture/logooo.svg'
-// import{Spheree} from './Spheree'
-import { Model } from './ModelDraco'
-// import {Model} from './3dmoond'
-// import{Model} from'./Nmdraco';
-import { degToRad } from 'three/src/math/MathUtils';
-import { Suspense } from 'react';
+
 import "./hero.css"
 import { Aboutmoon } from './Aboutmoon';
 export const Hero = () => {
-  const [colorr,setColorr]= useState('cyan');
- function changecolor(para){
-      setColorr(para);
-  }
+
+  
 
   return (
     <div className="hero-con">
       <div className='hero'>
-        <Canvas  >
-          {/* <pointLight position={[0,0,0]} intensity={1} color="cyan" castShadow/> */}
-
-          {/* <ambientLight intensity={1} /> */}
-          <Suspense fallback={null}>
-            <Model ref={Model }  colorr={colorr}/>
-            {/* <Model/> */}
-          </Suspense>
-        </Canvas>
+        
       </div>
       <div className="logotyp-con">
-        <div className="logo1" onMouseOver={()=>{ changecolor('#00ccff')}}  onMouseOut={()=>{changecolor('cyan')}}>
+        <div className="logo1" >
           <img src='./images/logomono.png'  class="img-fluid rounded mx-auto d-block p-2"  alt="" />
         </div>
-        <div className="typ" onMouseOver={()=>{ changecolor('#00ccff')}}  onMouseOut={()=>{changecolor('cyan')}}>
+        <div className="typ" >
           <Aboutmoon/>
         </div>
       </div>
