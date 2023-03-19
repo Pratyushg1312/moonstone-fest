855b6db8b702de945a1ad2373f73a84514b37ec8
sharma39vishal <sharma39vishal@gmail.com>
2023-03-19 19:25:47 +0530

    our link

--- client/src/components/Pages/Our Team/Team.jsx
+++ client/src/components/Pages/Our Team/Team.jsx
@@ -7,11 +7,11 @@ export const Teams = () => {
     <>
     <div className='team-container'>
         <div className="team-body">
-          <Team image={"./images/dishi.jpg"} name={"Dishika Jain"}/>
-          <Team image={"./images/vishal.jpg"} name={"Vishal Sharma"}/>
-          <Team image={"./images/1.png"} name={"Kalash Jain"}/>
-          <Team image={"./images/1.png"} name={"Pratyush Gupta"}/>
-          <Team image={"./images/mukta.jpg"} name={"Mukta Gupta"}/>
+          <Team image={"./images/dishi.jpg"} name={"Dishika Jain"} icon1={"fa-brands fa-linkedin fa-2x"} icon2={"fa-brands fa-github fa-2x"} icon3={"fa-brands fa-instagram fa-2x"} link1={""} link2={""} link3={""} desc={"Frontend Developer"}/>
+          <Team image={"./images/vishal.jpg"} name={"Vishal Sharma"} icon1={"fa-brands fa-linkedin fa-2x"} icon2={"fa-brands fa-github fa-2x"} icon3={"fa-solid fa-envelope fa-2x"} link1={"https://www.linkedin.com/in/sharma39vishal/"} link2={"https://github.com/sharma39vishal"} link3={"mailto:sharma39vishal@gmail.com"} desc={"Full Stack"}/>
+          <Team image={"https://media.licdn.com/dms/image/D4D35AQGQ8TLBvTkHyQ/profile-framedphoto-shrink_800_800/0/1665211305854?e=1679839200&v=beta&t=q_AoIxuISDm2gbHkPZEcffORqYsyvgBeWUQyRv5ZRmc"} name={"Kalash Jain"} icon1={"fa-brands fa-linkedin fa-2x"} icon2={"fa-brands fa-github fa-2x"} icon3={"fa-solid fa-envelope fa-2x"} link1={""} link2={""} link3={""} desc={"Frontend Developer"}/>
+          <Team image={"./images/1.png"} name={"Pratyush Gupta"} icon1={"fa-brands fa-linkedin fa-2x"} icon2={"fa-brands fa-github fa-2x"} icon3={"fa-brands fa-instagram fa-2x"} link1={""} link2={""} link3={""} desc={"Frontend Developer"}/>
+          <Team image={"./images/mukta.jpg"} name={"Mukta Gupta"} icon1={"fa-brands fa-linkedin fa-2x"} icon2={"fa-brands fa-github fa-2x"} icon3={"fa-brands fa-instagram fa-2x"} link1={""} link2={""} link3={""} desc={"Frontend Developer"}/>
         </div>
       </div>
       {/* <div className="profile-card-body">
