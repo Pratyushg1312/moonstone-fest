702d584f892b6a9ce9d86cba0036c7be877eacdf
jain-dishika <87193284+jain-dishika@users.noreply.github.com>
2023-03-19 13:52:42 +0530

    update

--- client/src/components/Pages/Parallax/NightEvent.js
+++ client/src/components/Pages/Parallax/NightEvent.js
@@ -9,7 +9,8 @@ const NightEvent = (props) => {
         <div className="card-bunch-child">
           {props.DayNum === 1 ? <><SoloEventCard imagefront={"/images/bandupper.jpg"} imageBack={"/images/bandupper.jpg"} />
           </> : (props.DayNum === 2 ? <><SoloEventCard imagefront={"/images/fashion2.jpg"} imageBack={"/images/fashion2.jpg"} />
-          <SoloEventCard imagefront={"/images/2.jpg"} imageBack={"/images/2.jpg"} /></> : <><SoloEventCard imagefront={"https://im.whatshot.in/img/2018/Oct/shutterstock-1009040557-1539776465.jpg"} imageBack={"https://im.whatshot.in/img/2018/Oct/shutterstock-1009040557-1539776465.jpg"} /></>)}
+          <SoloEventCard imagefront={"/images/2.jpg"} imageBack={"/images/2.jpg"} /></> : <><SoloEventCard imagefront={"https://im.whatshot.in/img/2018/Oct/shutterstock-1009040557-1539776465.jpg"} imageBack={"https://im.whatshot.in/img/2018/Oct/shutterstock-1009040557-1539776465.jpg"} />
+          <SoloEventCard imagefront={"/images/bandEvent.jpg"} imageBack={"/images/bandEvent.jpg"} /></>)}
         </div>
       </div>
     </section>
