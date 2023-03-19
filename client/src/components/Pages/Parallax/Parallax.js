e48579f12d0f33f559fa54e5e6d2576d7a1d8c66
Kalash Jain <kalashjain446@gmail.com>
2023-03-19 21:17:45 +0530

    new updation

--- client/src/components/Pages/Parallax/Parallax.js
+++ client/src/components/Pages/Parallax/Parallax.js
@@ -6,11 +6,11 @@ import "./Parallax.css"
 function Parallax() {
     return (
         <>
-            {/* <div className='d-flex justify-content-end me-2' style={{ position: "fixed", bottom: "0px", right: "0px" }}>
+            <div className='d-flex justify-content-end me-2' style={{ position: "fixed", bottom: "58px", right: "5px" }}>
                 <NavLink to="/map" className="floatingButton">
                     <img src="videos/telescope.gif" style={{ borderRadius: "50%" }} />
                 </NavLink>
-            </div> */}
+            </div>
             <DayEvent DayNum={1} id="DayOne"/>
             <NightEvent DayNum={1} />
             <DayEvent DayNum={2} />
