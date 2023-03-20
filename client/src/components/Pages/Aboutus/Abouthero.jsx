f707997e5197ab0a25bcb90bcb75dea88ab1604d
Kalash Jain <kalashjain446@gmail.com>
2023-03-21 00:45:45 +0530

    changes

--- client/src/components/Pages/Aboutus/Abouthero.jsx
+++ client/src/components/Pages/Aboutus/Abouthero.jsx
@@ -5,6 +5,28 @@ export default function Abouthero() {
     <div className="herobody my-5">
       <div className="about-hero-section">
         <div className="about-container">
+        <div className="moonstone-content">
+      <h1  class="txtanime"style={{ textAlign: "center" ,color:'rgb(197 178 0)' }}>About Moonstone </h1>
+            <h5 style={{color:"#56ffc7",textAlign:"center"}}>
+              Medicaps University is a distinguished educational institution
+              that has been providing quality education for 22 years. The
+              university is known for promoting research, encouraging innovative
+              entrepreneurship, and offering global exposure to its students.
+              The institution is devoted to providing students with a
+              comprehensive education that prepares them for success in their
+              future endeavours. The annual college festival, Moonstone, is
+              eagerly awaited by the students. The festival celebrates the
+              talent, creativity, and enthusiasm of the students. The students
+              come together to exhibit their diverse skills and passions,
+              creating an unforgettable experience for everyone involved. A
+              variety of events have been planned for the students, including
+              cultural performances, literary competitions, sports events, art
+              exhibits, and more. All students are cordially invited to partake
+              in this exhilarating weekend filled with entertainment, learning,
+              and discovery. Let us celebrate the power of community and the
+              spirit of creativity that the university epitomises. 
+            </h5>
+          </div>
           <section id="solar-system">
             <div id="sun"></div>
             <article id="mercury-trajectory">
@@ -25,28 +47,7 @@ export default function Abouthero() {
             <article id="uranus-trajectory"></article>
             <article id="neptune-trajectory"></article>
           </section>
-          <div className="moonstone-content">
-      <h1 style={{ textAlign: "center" ,color:'#07D685' }}>About Moonstone </h1>
-            <h5 style={{color:"#56ffc7"}}>
-              Medicaps University is a distinguished educational institution
-              that has been providing quality education for 22 years. The
-              university is known for promoting research, encouraging innovative
-              entrepreneurship, and offering global exposure to its students.
-              The institution is devoted to providing students with a
-              comprehensive education that prepares them for success in their
-              future endeavours. The annual college festival, Moonstone, is
-              eagerly awaited by the students. The festival celebrates the
-              talent, creativity, and enthusiasm of the students. The students
-              come together to exhibit their diverse skills and passions,
-              creating an unforgettable experience for everyone involved. A
-              variety of events have been planned for the students, including
-              cultural performances, literary competitions, sports events, art
-              exhibits, and more. All students are cordially invited to partake
-              in this exhilarating weekend filled with entertainment, learning,
-              and discovery. Let us celebrate the power of community and the
-              spirit of creativity that the university epitomises. 
-            </h5>
-          </div>
+          
         </div>
       </div>
     </div>
--- client/src/components/Pages/Aboutus/Aboutus.jsx
+++ client/src/components/Pages/Aboutus/Aboutus.jsx
@@ -11,19 +11,19 @@ export default function Aboutus() {
         <Abouthero />
       </div>
 
-      <div className=" row " style={{ margin: "0px", justifyContent: "center" }} >
+      <div className=" d-flex align-items-center row " style={{ margin: "0px", justifyContent: "center" }} >
         <div className='col-12 col-md-6 ' >
           <img src="https://www.dialeducation.com/assets/images/gallery/gallery_1625127758.jpg" alt="university's image" style={{ height: "80%", width: "100%" }} />
         </div>
-        <div className="col-10 col-md-6 ">
+        <div className="col-12 col-md-6 ">
 
           <div className="text-center my-4">
-            <h1 style={{ color: '#07D685' }} > Medi-Caps University</h1>
+            <h1 style={{ color: 'rgb(197 178 0)',fontSize:"50px",letterSpacing:"2px" }} > Medi-Caps University</h1>
           </div>
           <h5 className="text-center" style={{ color: "#56ffc7" }}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis velit labore placeat reprehenderit quidem omnis aliquam porro praesentium earum enim. Sapiente, quam. Doloribus sit est cupiditate, doloremque ut harum quisquam perferendis. Repellat, eum vero. Omnis accusamus sed odio, sequi facilis minus sint reiciendis nihil deserunt dolor exercitationem, quasi numquam et soluta modi vel impedit consectetur. Vitae at illo eveniet nihil modi consequatur laudantium? At perspiciatis unde nulla voluptate placeat corrupti non, asperiores facere ipsam distinctio error obcaecati nemo voluptates ipsum fugit omnis. Tempora recusandae .</h5>
         </div>
       </div>
-      <div className='row m-2'>
+      <div className='row m-4'>
         <div className='d-flex flex-wrap align-items-center justify-content-center '>
           <div className='md-col col-3 m-3 text-center'><img src="/images/ranked1st.png" style={{ width: "auto",height:"50%" }} /></div>
           <div className='md-col col-3 m-3 text-center'><img src="/images/mhrd-nirf.png" style={{ width: "auto",height:"50%"}} /></div>
@@ -32,7 +32,7 @@ export default function Aboutus() {
         </div>
       </div>
       <div className="text-center my-4">
-        <h1 style={{ color: '#07D685' }}  >Officials</h1>
+        <h1 style={{ color: 'rgb(197 178 0)', fontSize:"50px",letterSpacing:"2px"}}  >Officials</h1>
       </div>
       <div className='container '>
         <Persons />
