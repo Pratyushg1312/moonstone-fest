ea5af68019537595819b53464c6108e52c60d7f3
sharma39vishal <sharma39vishal@gmail.com>
2023-03-20 08:11:35 +0530

    h

--- client/src/components/Pages/Our Team/SinglePage.jsx
+++ client/src/components/Pages/Our Team/SinglePage.jsx
@@ -3,7 +3,6 @@ import "./Team.css"
 
 export const Team=(props) => {
   return (
-    
     <div className='profile-cards'>
         <div className="profile-card-header" >
             <div className="profile-image" >
@@ -19,9 +18,9 @@ export const Team=(props) => {
         <div className="profile-card-body">
             
             <ul class="icons clearfix">
-            <li><a href="#"><i class="fa fa-brands fa-facebook-f" aria-hidden="true"></i></a></li>
-            <li><a href="#"><i class="fa fa-brands fa-twitter"></i></a></li>
-            <li><a href="#"><i class="fa fa-brands fa-linkedin-in"></i></a></li>
+            <li><a href={props.link1}><i class={props.icon1} aria-hidden="true"></i></a></li>
+            <li><a href={props.link2}><i class={props.icon2}></i></a></li>
+            <li><a href={props.link3}><i class={props.icon3}></i></a></li>
           </ul>
             
 
--- client/src/components/Pages/Parallax/DayEvent.js
+++ client/src/components/Pages/Parallax/DayEvent.js
@@ -7,7 +7,7 @@ const DayEvent = (props) => {
       <h2>Day {props.DayNum}<i class="fa-solid fa-sun" style={{ color: "#fab005" }}></i></h2>
       <div className="card-bunch">
         <div className="card-bunch-child">
-        {props.DayNum === 1 ? <SoloEventCard imagefront={"/images/4.jpg"} imageBack={"/images/4.jpg"} name={"Inauguration"}/>
+        {props.DayNum === 1 ? <SoloEventCard imagefront={"/images/4.jpg"} imageBack={"/images/4.jpg"} name={"Begin"}/>
            : (props.DayNum === 2 ? <><SoloEventCard imagefront={"/images/3.jpg"} imageBack={"/images/3.jpg"} name={"NSS"}/>
           </> : <><SoloEventCard imagefront={"/images/3.jpg"} imageBack={"/images/3.jpg"} name={"Sports Event"}/>
           <SoloEventCard imagefront={"/images/10.jpg"} imageBack={"/images/10.jpg"} name={"Sports Event"}/>
