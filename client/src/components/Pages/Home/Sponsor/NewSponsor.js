051ab995872e1b38e4b5e7a2aecbfdcafb728c8a
Kalash Jain <kalashjain446@gmail.com>
2023-03-24 12:18:44 +0530

    some changes 2

--- client/src/components/Pages/Home/Sponsor/NewSponsor.js
+++ client/src/components/Pages/Home/Sponsor/NewSponsor.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './NewSponsor.css';
+
 export default function NewSponsor() {
     return (
         <div className="sponsor-section" style={{ color: "white" }}>
@@ -10,12 +11,9 @@ export default function NewSponsor() {
                         <p className='start-0'>Our sponsors include architects, engineers, contractors, material suppliers, building owners, policymakers, nonprofits, and government organizations, providing a 360-degree view of the challenges we face in the built environment. Their insights and resources are invaluable in accelerating action, in our highly collaborative environment.</p>
                     </div>
                     <div className="scrollable-column">
-                        <div className='col-md-7  col-12 text-center'><img src="./images/25year.png"   style={{width:"50%"}}/><p>Company Name</p><p>Title parterner</p></div>                  
-                        <div className='col-md-7 col-12 text-center'><img src="./images/25year.png"   style={{width:"50%"}}/><p>Company Name</p><p>Title parterner</p></div>                  
-                        <div className='col-md-6 col-12 text-center'><img src="./images/25year.png"   style={{width:"50%"}}/><p>Company Name</p><p>Title parterner</p></div>                  
-                        <div className='col-md-6 col-12 text-center'><img src="./images/25year.png"   style={{width:"50%"}}/><p>Company Name</p><p>Title parterner</p></div>                  
-                        <div className='col-md-6 col-12 text-center'><img src="./images/25year.png"   style={{width:"50%"}}/><p>Company Name</p><p>Title parterner</p></div>                  
-                        <div className='col-md-6 col-12 text-center'><img src="./images/25year.png"   style={{width:"50%"}}/><p>Company Name</p><p>Title parterner</p></div>                  
+                        <div className='col-md-12  col-12 text-center pq' ><img src="./images/25year.png"  /><p>Company Name</p><p>Title parterner</p></div>                  
+                        <div className='col-md-12 col-12 text-center pq' ><img src="./images/25year.png"  /><p>Company Name</p><p>Title parterner</p></div>                  
+                      
                         <div className='col-md-4 col-12 text-center'><img src="./images/25year.png"   style={{width:"50%"}}/><p>Company Name</p><p>Title parterner</p></div>                  
                         <div className='col-md-4 col-12 text-center'><img src="./images/25year.png"   style={{width:"50%"}}/><p>Company Name</p><p>Title parterner</p></div>                  
                         <div className='col-md-4 col-12 text-center'><img src="./images/25year.png"   style={{width:"50%"}}/><p>Company Name</p><p>Title parterner</p></div>                  
