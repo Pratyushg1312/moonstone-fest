effed77801768f2c466eb7ee55ea2ef732d72242
Mukta <mukta.gupta08@gmail.com>
2023-03-25 22:40:19 +0530

    glimpses page

--- client/src/components/Pages/Glimpses/Glimpses.jsx
+++ client/src/components/Pages/Glimpses/Glimpses.jsx
@@ -17,33 +17,33 @@ export default function Glimpses() {
         
         
         <div className="item"><img src="./images/10.jpg" alt="" /></div>
-        <div className="tall"><img src="./images/1.jpg" alt="" /></div>
+        <div className="tall"><img src="./images/2.jpg" alt="" /></div>
         <div className="big"><img src="./images/4.jpg" alt="" /></div>
         <div className="tall"><img src="./images/12.jpg" alt="" /></div>
         <div className="item"><img src="./images/9.jpg" alt="" /></div>
-        <div className="wide"><img src="./images/3.jpg" alt="" /></div>
+        <div className="item"><img src="./images/3.jpg" alt="" /></div>
         
-        <div className="wide"><img src="./images/14.jpg" alt="" /> </div>
+        <div className="wide"><img src="./images/1.jpg" alt="" /> </div>
         <div className="big"><img src="./images/15.jpg" alt="" /></div>
         <div className="item"><img src="./images/16.jpg" alt="" /> </div>
-        <div className="tall"><img src="./images/17.jpg" alt="" /> </div>
+        <div className="tall"><img src="./images/20.jpg" alt="" /> </div>
         
-        <div className="big"><img src="./images/14.jpg" alt="" /></div>
+        <div className="big"><img src="./images/25.jpg" alt="" /></div>
         <div className="tall"><img src="./images/15.jpg" alt="" /></div>
-        <div className="wide"><img src="./images/18.jpg" alt="" /></div>
-        <div className="wide"><img src="./images/1.jpg" alt="" /></div>
+        <div className="item"><img src="./images/27.jpg" alt="" /></div>
+        <div className="big"><img src="./images/23.jpg" alt="" /></div>
 
         <div className="item"><img src="./images/17.jpg" alt="" /></div>
-        <div className="tall"><img src="./images/16.jpg" alt="" /></div>
-        <div className="big"><img src="./images/24.jpg" alt="" /></div>
-        <div className="tall"><img src="./images/2.jpg" alt="" /></div>
-        <div className="item"><img src="./images/20.jpg" alt="" /></div>
-        <div className="wide"><img src="./images/22.jpg" alt="" /></div>
-        <div className="item"><img src="./images/26.jpg" alt="" /></div>
-        <div className="big"><img src="./images/27.jpg" alt="" /></div>
-        <div className="big"><img src="./images/25.jpg" alt="" /></div>
+        {/* <div className="tall"><img src="./images/16.jpg" alt="" /></div> */}
+        <div className="item"><img src="./images/24.jpg" alt="" /></div>
+        {/* <div className="tall"><img src="./images/.jpg" alt="" /></div> */}
         <div className="tall"><img src="./images/21.jpg" alt="" /></div>
-        <div className="item"><img src="./images/20.jpg" alt="" /></div>
+        <div className="wide"><img src="./images/22.jpg" alt="" /></div>
+        <div className="item"><img src="./images/28.jpg" alt="" /></div>
+        <div className="wide"><img src="./images/30.jpg" alt="" /></div>
+        <div className="item"><img src="./images/29.jpg" alt="" /></div>
+        {/* <div className="tall"><img src="./images/21.jpg" alt="" /></div> */}
+        {/* <div className="item"><img src="./images/20.jpg" alt="" /></div> */}
         {/* <div className="item"><img src="./images/23.jpg" alt="" /></div> */}
       </div>
       </div>
