da7fde0ac4694c8c3ebbfd4825f04c1485d62f70
Mukta <mukta.gupta08@gmail.com>
2023-04-01 18:01:16 +0530

    added night events

--- client/src/components/Pages/EventPage/EventCategory.js
+++ client/src/components/Pages/EventPage/EventCategory.js
@@ -10,7 +10,7 @@ function EventCategory(props) {
           <h2>{props.event_name}</h2>
           <p>{props.event_desc}</p>
           <div className='d-flex'>
-            {/* <NavLink to={props.event_link}><button className='view-event-button mx-2'>View</button></NavLink> */}
+            <NavLink to={props.event_link}><button className='view-event-button mx-2'>View</button></NavLink>
             <NavLink to="/registration"><button className="view-event-button mx-2"><span>Register</span></button></NavLink>
           </div>
         </div>
